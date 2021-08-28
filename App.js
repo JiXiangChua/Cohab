ccf49ab0b7c451c3613b6c318f00ab3e22f4054c
JiXiangChua <jixiangchua2697@gmail.com>
2021-08-29 00:53:07 +0800

    Added Navigation from Login to Home screen

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { StyleSheet , View } from 'react-native';
-import LoginScreen from './components/LoginScreen.js';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { createStackNavigator, TransitionSpecs } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import LoginScreen from "./components/LoginScreen.js";
+import HomeScreen from "./components/HomeScreen.js";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginScreen />
-    </View>
+    // <View style={styles.container}>
+    //   <LoginScreen />
+    // </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,66 +1,80 @@
-import React , { useState } from 'react';
-import { StyleSheet , Text , TextInput , View , Image } from 'react-native';
-import logo from '../assets/logo.png';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/stack";
+import logo from "../assets/logo.png";
+import HomeScreen from "./HomeScreen";
 
-export default function LoginScreen() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    return(
-        <View style = {styles.container}>
-            <Image source={logo} style={styles.logo} /> 
-            <TextInput 
-                style={styles.inputField}
-                placeholder='Username/Email'
-                onChangeText={username => setUsername(username)}
-                defaultValue={username}
-            />
-            <TextInput 
-                style={styles.inputField}
-                placeholder='Password'
-                onChangeText={password => setPassword(password)}
-                defaultValue={password}
-            />
-            <View style = {styles.button}>
-                <Text style = {styles.buttonText}>Log In</Text>
-            </View>
-            <View style = {styles.button}>
-                <Text style = {styles.buttonText}>Sign In</Text>
-            </View>
+export default function LoginScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-        </View>
-    )
+  function login() {
+    navigation.replace("Home");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <TextInput
+        style={styles.inputField}
+        placeholder="Username/Email"
+        onChangeText={(username) => setUsername(username)}
+        defaultValue={username}
+      />
+      <TextInput
+        style={styles.inputField}
+        placeholder="Password"
+        onChangeText={(password) => setPassword(password)}
+        defaultValue={password}
+      />
+      <TouchableOpacity style={styles.button} onPress={login}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    logo: {
-        minHeight:100,
-        minWidth:150,
-        marginBottom:100,
-        resizeMode: 'center',
-    },
-    container: {
-        flex: 1,
-        backgroundColor: '#FFD692',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputField: {
-        marginTop: 15,
-        height: 40,
-        width: '75%',
-        borderBottomWidth: 1,
-        borderColor: '#6E2142',
-    },
-    button: {
-        marginTop: 30,
-        height: 40,
-        width: '75%',
-        backgroundColor: '#E16363',
-        justifyContent: 'center',
-    },
-    buttonText: {
-        color: '#FFD692',
-        textAlign: 'center',
-        fontSize: 18,
-    }
-  });
+  logo: {
+    minHeight: 100,
+    minWidth: 150,
+    marginBottom: 100,
+    resizeMode: "center",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#FFD692",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputField: {
+    marginTop: 15,
+    height: 40,
+    width: "75%",
+    borderBottomWidth: 1,
+    borderColor: "#6E2142",
+  },
+  button: {
+    marginTop: 30,
+    height: 40,
+    width: "75%",
+    backgroundColor: "#E16363",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFD692",
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
