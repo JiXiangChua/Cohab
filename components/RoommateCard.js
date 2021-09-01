1845392720d72e8c423e8ad18d7416336d3736f6
JiXiangChua <jixiangchua2697@gmail.com>
2021-09-01 15:40:10 +0800

    Added RoommateCard Component

---
+++ components/RoommateCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function RoommateCard() {
+  return (
+    <View style={styles.roommateCards}>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  roommateCards: {
+    backgroundColor: "#FFD897",
+    width: "100%",
+    height: 200,
+  },
+});
--- screens/FinanceScreen.js
+++ screens/FinanceScreen.js
@@ -9,6 +9,8 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import settingLogo from "../assets/Finance-assets/SettingsWheel.png";
+import addBillButton from "../assets/Finance-assets/AddBill.png";
+import RoommateCard from "../components/RoommateCard";
 
 export default function FinanceScreen() {
   const [budget, setBudget] = useState(100);
@@ -25,7 +27,8 @@ export default function FinanceScreen() {
           alignItems: "center",
         }}
       >
-        {/* <View style={styles.balanceContainer}> */}
+        {/* Balance Section */}
+
         <Text style={styles.balanceText}> Wallet </Text>
         <Text style={{ color: "#8A8585" }}> Current Balance </Text>
         <View style={styles.buttonContainer}>
@@ -33,7 +36,8 @@ export default function FinanceScreen() {
           <TouchableOpacity style={styles.walletButton}></TouchableOpacity>
           <TouchableOpacity style={styles.walletButton}></TouchableOpacity>
         </View>
-        {/* </View> */}
+
+        {/* Monthly Budget Section */}
 
         <View style={styles.budgetContainer}>
           <Text style={{ position: "absolute", left: 0, fontSize: 18 }}>
@@ -54,7 +58,45 @@ export default function FinanceScreen() {
           </TouchableOpacity>
         </View>
 
-        <View></View>
+        {/* Monthly Budget Display */}
+        <View>
+          <Text style={{ color: "#8A8585" }}> $ 710 / $1000</Text>
+        </View>
+
+        {/* Roommate Section */}
+        <View style={styles.roommateContainer}>
+          <View style={{ width: "100%", height: 50, flexDirection: "row" }}>
+            <Text
+              style={{
+                position: "absolute",
+                left: 0,
+                fontSize: 18,
+                marginTop: 7,
+              }}
+            >
+              Roommates
+            </Text>
+            <Text
+              style={{
+                position: "absolute",
+                right: 40,
+                fontSize: 18,
+                marginTop: 7,
+              }}
+            >
+              New Bill
+            </Text>
+            <TouchableOpacity
+              style={{ position: "absolute", right: 0, fontSize: 18 }}
+            >
+              <Image source={addBillButton}></Image>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.roommateBillPanel}>
+            {/* Display each roommate and how much  */}
+            <RoommateCard></RoommateCard>
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -105,5 +147,17 @@ const styles = StyleSheet.create({
     width: "90%",
     height: 20,
     borderRadius: 10,
+  },
+  roommateContainer: {
+    backgroundColor: "#888",
+    marginTop: 20,
+    width: "80%",
+    alignItems: "center",
+  },
+  roommateBillPanel: {
+    backgroundColor: "#FFF",
+    width: "90%",
+    height: 400,
+    borderRadius: 20,
   },
 });
