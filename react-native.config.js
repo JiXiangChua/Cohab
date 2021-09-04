047cdd5ab41f2f1350048a5de2e9965a9f913826
donocho <79370213+donocho@users.noreply.github.com>
2021-09-05 01:09:03 +0800

    Add files via upload

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    project: {
+      ios: {},
+      android: {},
+    },
+    // use "npx react-native link" in terminal and link
+    assets: ["./assets/FontResource/Montserrat/"],
+  };
