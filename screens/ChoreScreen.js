9159dda36d7d9e5cfe3c7a8345c539e9e19bdeaf
Justin <justinchongzhiyu@gmail.com>
2021-09-08 23:22:48 +0800

    Squashed commit of the following:
    commit 30f2e823611a012bd458141a1349499af2535b6a
    Author: donocho <neonshinjiikari@gmail.com>
    Date:   Wed Sep 8 21:45:06 2021 +0800
    
        ChoreScreen update
    
    commit 17b44897c6570610cf4f818c1c3484ea3e9b8b7f
    Author: donocho <neonshinjiikari@gmail.com>
    Date:   Tue Sep 7 00:27:26 2021 +0800
    
        updating more
    
    commit b5b5ee494e924aa3870e02fc5f49d2bc3cf10e7a
    Author: donocho <neonshinjiikari@gmail.com>
    Date:   Mon Sep 6 22:03:56 2021 +0800
    
        updating stuff
    
    commit 813e07162c24b522e45b2d2849537bf7fa9d40fb
    Author: donocho <neonshinjiikari@gmail.com>
    Date:   Sun Sep 5 02:20:19 2021 +0800
    
        commit
    
    commit 047cdd5ab41f2f1350048a5de2e9965a9f913826
    Author: donocho <79370213+donocho@users.noreply.github.com>
    Date:   Sun Sep 5 01:09:03 2021 +0800
    
        Add files via upload

--- screens/ChoreScreen.js
+++ screens/ChoreScreen.js
@@ -1,23 +1,57 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-} from "react-native";
+import React, { useState } from "react";
+import {View,Text,StyleSheet,TouchableOpacity,Image,ScrollView} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-import MenuBar from "../components/MenuBar";
+//header
+import NotificationButton from "../assets/NotificationButton.png";
+import backToRoomButton from "../assets/back-to-room-button.png";
+//assets
+import RoommateCard from "../components/ChoreCard";
+import NewChoresButton from "../assets/Chores-assets/Caddbutton.png";
 
 export default function ChoreScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <MenuBar navigation = {navigation} />
-      <ScrollView>
-        {/* Write Your code here */}
-        <Text>Hello to Chore Screen</Text>
+      <View style={styles.menuBarStyle}>
+        <TouchableOpacity
+          style={{ alignSelf: "flex-start", flexDirection: "row", right: 15 }}
+          onPress={navigation.goBack}
+        >
+          <Image source={backToRoomButton}></Image>
+          <Text style={styles.backToRoomButtonStyle}>home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ alignSelf: "flex-end" }}>
+          <Image source={NotificationButton}></Image>
+        </TouchableOpacity>
+      </View>
+      {/*stuff*/}
+      <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+        <Text style={styles.choreText}> Chores </Text>
+        <View style={styles.choreContainer}>
+          <Text style={styles.chorenewText}>New Chore</Text>
+          <TouchableOpacity style={{ position: "absolute", right: 30}}>
+            <Image source={NewChoresButton}></Image>
+          </TouchableOpacity>
+        </View>
+        {/* Roommate Bill Container */}
+      <View
+        style={{
+          width: "95%",
+          paddingVertical: 5,
+          alignItems: "center",
+        }}>
+        <RoommateCard
+          duedate="14/7"
+          dutyname="Wash the dishes"
+          description="It's your turn!"
+          choretype="Weekly"
+          status="True"
+        ></RoommateCard>
+      </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -42,5 +76,29 @@ const styles = StyleSheet.create({
     fontSize: 20,
     alignSelf: "center",
     right: 10,
+  },
+  scrollView: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  choreText: {
+    flexWrap: "wrap",
+    //fontFamily: "Montserrat-Regular",
+    color: "#E16363",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  choreContainer: {
+    width: "95%",
+    height: 30,
+    flexDirection: "row",
+  },
+  chorenewText: {
+    //fontFamily: "Montserrat-Regular",
+    position: "absolute", 
+    right: 60,
+    flexWrap: "wrap",
+    color: "#826335",
+    fontSize: 18,
   },
 });
