d89d7748a78df2007e92d8b6fc972e4f8be791fa
shutingpan <shutingpan@gmail.com>
2021-09-22 18:44:50 +0800

    created UI of Chore Screen popup (modal), havent implemented functionality yet

--- screens/ChoreScreen.js
+++ screens/ChoreScreen.js
@@ -1,14 +1,42 @@
 import React, { useState } from "react";
-import {View,Text,StyleSheet,TouchableOpacity,Image,ScrollView} from "react-native";
+import {View,Text, StyleSheet,TouchableOpacity,Image,ScrollView, Modal, TextInput, Button} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 //header
 import MenuBar from "../components/MenuBar";
 //assets
 import RoommateCard from "../components/ChoreCard";
-import NewChoresButton from "../assets/Chores-assets/Caddbutton.png";
+import NewChoreButton from "../assets/icons/icon_designs-03.png";
+import { render } from "react-dom";
 
+ 
 export default function ChoreScreen({ navigation }) {
+  
+  // When click on 'New Chore', popup (modal) appears
+  function AddNewChore() { 
+    //TO BE CODED
+  }
+
+  // Values needed for Add Chore Screen Popup (Modal)
+  var daysOfTheWeek = ["M", "T", "W", "T", "F", "S","S"];
+  var repeatOptions = ["Weekly", "Monthly"];
+  var displayDaysOfTheWeek = [];
+  var displayRepeatOptions = [];
+
+  // To display options to repeat weekly or monthly
+  for (let i=0; i<2;i++){
+    displayRepeatOptions.push(
+      <Button title= {repeatOptions[i]}/>
+    )
+  }
+  // To display days of the week
+  for (let i=0; i<7;i++){
+    displayDaysOfTheWeek.push(
+      <Button title= {daysOfTheWeek[i]}/>
+    )
+  }
+
   return (
+
     <SafeAreaView style={styles.container}>
       <MenuBar navigation = {navigation} />
       {/*stuff*/}
@@ -19,12 +47,14 @@ export default function ChoreScreen({ navigation }) {
         alignItems: "center",
       }}>
         <Text style={styles.choreText}> Chores </Text>
+        
         <View style={styles.choreContainer}>
           <Text style={styles.chorenewText}>New Chore</Text>
-          <TouchableOpacity style={{ position: "absolute", right: 30}}>
-            <Image source={NewChoresButton}></Image>
+          <TouchableOpacity style={{ position: "absolute", right: 0, top: -5 }}>
+            <Image style={styles.newChoreButton} onPress={()=>{AddNewChore}}source={NewChoreButton}></Image>
           </TouchableOpacity>
         </View>
+        
         {/* Roommate Bill Container */}
       <View
         style={{
@@ -40,10 +70,55 @@ export default function ChoreScreen({ navigation }) {
           status="True"
         ></RoommateCard>
       </View>
+
+
+        {/*'New Chore' Popup Screen (Modal) */}
+        <Modal transparent = {true} visible= {true} >
+          {/*Rest of screen will darken*/}
+          <View style={{backgroundColor: "#000000aa", flex: 1}}>
+            {/*Pop up screen will show*/}
+            <View style={{backgroundColor: "#ffffff", margin: 30, padding: 30, borderRadius: 10}}> 
+              {/*Type in Chore name*/}
+              <TextInput 
+              style={{ borderBottomWidth: 0.5, width: "50%"}}
+              placeholder="Chore Title"
+              />
+              {/*Type in Chore description*/}
+              <TextInput 
+              style={{paddingTop: 10, borderBottomWidth: 0.5}}
+              placeholder="Description"
+              />
+
+
+              <Text style={{paddingTop:20}}>Select the order</Text>
+
+
+              <Text style={{paddingTop:20}}>Repeat</Text>
+
+              <View style={{ paddingTop:20, flexDirection: "row", marginHorizontal: 10, justifyContent: 'space-around'}}>
+              {displayRepeatOptions}
+              </View>
+
+
+              <Text style={{paddingTop:20}}>Select the day</Text>
+
+              <View style={{ paddingTop:20, flexDirection: "row", marginHorizontal: 10, justifyContent: 'space-around'}}>
+              {displayDaysOfTheWeek}
+              </View>
+
+            </View>
+          </View>
+        </Modal>
+
+        {/*END OF 'New Chore' Popup Screen (Modal) */}
+
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -81,8 +156,15 @@ const styles = StyleSheet.create({
     height: 30,
     flexDirection: "row",
   },
+
+
+  newChoreButton: {
+    width: 40,
+    height: 40,
+  },
+
   chorenewText: {
-    //fontFamily: "Montserrat-Regular",
+    //fontFamily: "Montserrat-SemiBold",
     position: "absolute", 
     right: 60,
     flexWrap: "wrap",
