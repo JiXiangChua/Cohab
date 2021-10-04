5a65f9dfc7f1cf463390af5592adc569c95ef527
Zencron <justinchongzhiyu@gmail.com>
2021-10-04 20:42:20 +0800

    cleanup

--- react-native.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    project: {
-      ios: {},
-      android: {},
-    },
-    // use "npx react-native link" in terminal and link
-    assets: ["./assets/FontResource/Montserrat/"],
-  };
