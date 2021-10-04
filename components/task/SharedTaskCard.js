cd89f331c0b3f03deac96b3c0e73bd0a17f444c1
Zencron <justinchongzhiyu@gmail.com>
2021-10-04 22:01:07 +0800

    rachel's screens

---
+++ components/task/SharedTaskCard.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Touchable,
+  TouchableOpacity,
+  ScrollView,
+  Modal,
+  Pressable,
+} from "react-native";
+
+import ProfilePic from "../../assets/Finance-assets/Kimberly.png";
+
+export default function SharedTaskCard(props) {
+  return (
+    <View
+      style={[styles.card, styles.shadowProp, { justifyContent: "center" }]}
+    >
+      <View style={{ flexDirection: "row" }}>
+        {/* Column for Name, Description & buttons */}
+        <View style={styles.cardDisplayColumnFormat}>
+          <Text style={styles.cardText}>{props.name}</Text>
+          <Text style={[styles.cardText, { fontSize: 14, color: "#8A8585" }]}>
+            {props.description}
+          </Text>
+          {/* Edit button */}
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity style={[styles.editButton, styles.shadowProp]}>
+              <Text style={{ color: "#FFF", fontSize: 16 }}>Edit</Text>
+            </TouchableOpacity>
+            {/* Done button */}
+            <TouchableOpacity style={[styles.claimButton, styles.shadowProp]}>
+              <Text style={{ color: "#FFF", fontSize: 16 }}>Done</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Column for set deadline  */}
+        <View style={[styles.cardDisplayColumnFormat, { marginLeft: 5 }]}>
+          <Text style={[styles.deadlineText, { marginRight: 10 }]}>
+            {props.deadline}
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={[styles.cardText, { fontSize: 15, color: "#8A8585" }]}>
+              {props.date_created}
+            </Text>
+            <Image source={ProfilePic} style={styles.profileImage} />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+    width: "105%",
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "#0038FF",
+  },
+  shadowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+  },
+  cardDisplayColumnFormat: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+  },
+  cardText: {
+    alignSelf: "flex-start",
+    marginRight: 20,
+    fontSize: 18,
+  },
+  deadlineText: {
+    alignSelf: "center",
+    marginRight: 10,
+    fontSize: 14,
+    color: "#FF0000",
+    marginBottom: 45,
+  },
+  editButton: {
+    backgroundColor: "#0038FF",
+    height: 38,
+    width: 100,
+    borderRadius: 10,
+    marginRight: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  claimButton: {
+    backgroundColor: "#36BC7C",
+    height: 38,
+    width: 100,
+    borderRadius: 10,
+    marginRight: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  profileImage: {
+    marginLeft: -15,
+    minWidth: 15,
+    minHeight: 15,
+    marginTop: -20,
+    alignSelf: "center",
+  },
+});
