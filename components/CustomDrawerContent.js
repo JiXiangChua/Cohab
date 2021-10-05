ea689a4f3c409076b3c61a9fb325638ecd082838
Justin <justinchongzhiyu@gmail.com>
2021-10-06 00:57:55 +0800

    remove unwanted items from drawer list

--- components/CustomDrawerContent.js
+++ components/CustomDrawerContent.js
@@ -3,14 +3,24 @@ import { View, Text, StyleSheet } from "react-native";
 import {
   DrawerContentScrollView,
   DrawerItemList,
+  DrawerItem,
 } from "@react-navigation/drawer";
 
-export default function CustomDrawerContent(props) {
+export default function CustomDrawerContent({ navigation }) {
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
+    <DrawerContentScrollView>
+      <DrawerItem label="Home" onPress={() => navigation.navigate('Home')} labelStyle = {styles.drawerLabel} />
+      <DrawerItem label="Calendar" onPress={() => navigation.navigate('Calendar')} labelStyle = {styles.drawerLabel} />
+      <DrawerItem label="Chore" onPress={() => navigation.navigate('Chore')} labelStyle = {styles.drawerLabel} />
+      <DrawerItem label="Finance" onPress={() => navigation.navigate('Finance')} labelStyle = {styles.drawerLabel} />
+      <DrawerItem label="Task" onPress={() => navigation.navigate('Task')} labelStyle = {styles.drawerLabel} />
     </DrawerContentScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  drawerLabel: {
+    fontFamily: "Montserrat",
+    color: "#FFD897",
+  },
+});
