eeda05ea88c98075b53b696ea3403331db1bf3c8
Remicchi <80085164+Remicchi@users.noreply.github.com>
2021-10-20 19:59:40 +0800

    Added Framework for Notifications Over Navigation

--- components/CustomDrawerContent.js
+++ components/CustomDrawerContent.js
@@ -1,14 +1,61 @@
 import React from "react";
-import { View , Text , StyleSheet , Image , TouchableOpacity } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Modal,
+  Button,
+  Pressable,
+  TextInput,
+} from "react-native";
 import {
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
 } from "@react-navigation/drawer";
 
+import BasicText from "../components/BasicText.js";
+import Card from "../navigation/genericNotifCard.js";
+import ProfilePic from "../assets/Finance-assets/Kimberly.png";
 import profileImage from '../assets/Calendar-assets/ProfileImage2.png';
 
 export default function CustomDrawerContent({ navigation }) {
+
+  var statusCheckInOrOut = ["Check In", "Check Out"];
+  var displayCheckIn = [];
+
+  // Variables to indicate if check in or out
+    var checkedIn = false;
+    var checkedOut = false;
+
+    // Change when user checks in or out
+    const [checkInOrOut, setStatus] = useState("Check In");
+
+    if (checkInOrOut == "Check In") {
+      checkedOut = false;
+      checkedIn = true;
+    } else if (checkInOrOut == "Check Out") {
+      checkedOut = true;
+      checkedIn = false;
+    }
+
+    for (let i = 0; i < 2; i++) {
+      displayCheckIn.push(
+        <Pressable
+          key={i}
+          onPress={() => setStatus(statusCheckInOrOut[i])}
+          style={[styles.checkInButton, styles.checkInClose, {backgroundColor: ( checkInOrOut == statusCheckInOrOut[i] ? "grey" : "#2196F3")}]}
+        >
+          <BasicText style={[styles.checkInButtonText, {color: (checkInOrOut == statusCheckInOrOut[i] ? "#A9A9A9" : "white")}]}>
+            {statusCheckInOrOut[i]}
+          </BasicText>
+        </Pressable>
+      );
+    }
 
   function handleEditProfile() {
     console.log('edit profile');
@@ -19,14 +66,49 @@ export default function CustomDrawerContent({ navigation }) {
   }
 
   return (
+
+
     <DrawerContentScrollView contentContainerStyle = {styles.container}>
       <View style = {styles.topContainer}>
-        <DrawerItem label="Home" onPress={() => navigation.navigate('Home')} labelStyle = {styles.drawerLabel} />
-        <DrawerItem label="Calendar" onPress={() => navigation.navigate('Calendar')} labelStyle = {styles.drawerLabel} />
-        <DrawerItem label="Chore" onPress={() => navigation.navigate('Chore')} labelStyle = {styles.drawerLabel} />
-        <DrawerItem label="Finance" onPress={() => navigation.navigate('Finance')} labelStyle = {styles.drawerLabel} />
-        <DrawerItem label="Task" onPress={() => navigation.navigate('Task')} labelStyle = {styles.drawerLabel} />
+        <Text style = {styles.alignCenter}>Notification</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 35,
+            justifyContent: "space-around",
+          }}
+        >
+          {displayCheckIn}
+        </View>
+        <View style={{ flexDirection: "row" }}>
+            <Image source = {profileImage} style = {styles.profileImage} />
+            <Image source = {profileImage} style = {styles.profileImage} />
+            <Image source={ProfilePic} style={styles.profileImage} />
+        </View>
+            <Text style = {styles.alignCenter}>Announcements</Text>
+            <View style={styles.notifContainer}>
+                <Card
+                    name="What is ligma?"
+                    date_created="18 Aug"
+                    fdCheck={false}
+                />
+            </View>
+        <Text style = {styles.alignCenter}>Events</Text>
+        <View style={styles.notifContainer}>
+            <Card
+              name="What is ligma?"
+              date_created="18 Aug"
+              fdCheck={false}
+            />
+            <Card
+              name="What is ligma?"
+              date_created="18 Aug"
+               fdCheck={false}
+            />
+        </View>
+
       </View>
+
       <View style = {styles.bottomContainer}>
         <View style = {styles.alignRight}><Image source = {profileImage} style = {styles.profileImage} /></View>
         <TouchableOpacity onPress = {handleEditProfile}><Text style = {styles.alignRight}>Edit Profile</Text></TouchableOpacity>
@@ -50,6 +132,15 @@ const styles = StyleSheet.create({
     fontFamily: "Montserrat",
     color: "#FFD897",
   },
+  alignCenter: {
+    width: '90%',
+    fontFamily: "Montserrat",
+    textAlign: 'center',
+    color: "#FFD897",
+    flexDirection: 'row-reverse',
+    marginTop: 10,
+    fontSize: 20,
+  },
   alignRight: {
     width: '90%',
     fontFamily: "Montserrat",
@@ -59,7 +150,36 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   profileImage: {
-    width: 40,
-    height: 40,
-  }
+    marginTop: 5,
+    marginLeft: 5,
+    width: 50,
+    height: 50,
+  },
+  notifContainer: {
+    width: "95%",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+  },
+
+  checkInButton: {
+    borderRadius: 20,
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "#A9A9A9",
+    backgroundColor: "white",
+    padding: 5,
+    elevation: 2,
+    marginTop: 10,
+    marginBottom: 10,
+    },
+
+  checkInButtonText: {
+    color: "#2196F3",
+    fontFamily: 'MontserratBold',
+    textAlign: "center",
+  },
+
+  checkInClose: {
+      minWidth: 100,
+  },
 });
