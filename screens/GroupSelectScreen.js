0d19cd95e1cc0dbd0f9d70f9c05a6d048d13b828
rachel <RACH0102@e.ntu.edu.sg>
2021-11-01 14:39:04 +0800

    Changed dog avatar for group screen

--- screens/GroupSelectScreen.js
+++ screens/GroupSelectScreen.js
@@ -6,7 +6,7 @@ import { BasicText } from "../components";
 import ProfilePicture from "../assets/Finance-assets/Kimberly.png";
 import BuildingPicture from "../assets/Building.png";
 import AddButtonLogo from "../assets/Finance-assets/AddButton.png";
-import dog1Gif from "../assets/Home-assets/dog1.gif";
+import dog1Gif from "../assets/Home-assets/doggo.gif";
 import DayBackground from "../assets/Group-assets/DayBG.png";
 import EveningBackground from "../assets/Group-assets/EveningBG.png";
 import NightBackground from "../assets/Group-assets/NightSkyBG.png";
@@ -179,7 +179,7 @@ export function GroupSelectScreen(props) {
             width: 30,
             height: 130,
             position: "absolute",
-            top: 610,
+            top: 670,
             left: 155,
           }}
           onPress={() => {
@@ -192,8 +192,8 @@ export function GroupSelectScreen(props) {
             source={dog1Gif}
             style={{
               resizeMode: "contain",
-              width: 200,
-              height: 200,
+              width: 180,
+              height: 80,
               alignSelf: "center",
               shadowColor: "#FFF",
               shadowOffset: { width: -2, height: 3 },
@@ -217,6 +217,33 @@ export function GroupSelectScreen(props) {
               style={[styles.arrowContainer, styles.arrowRightContainer]}
             ></View>
           </View>
+
+          {/* New speech bubble */}
+          {/* <View
+          style={{
+            backgroundColor: "#0078fe",
+            padding: 10,
+            marginLeft: "45%",
+            borderRadius: 5,
+            //marginBottom: 15,
+            marginTop: 5,
+            marginRight: "5%",
+            maxWidth: "50%",
+            alignSelf: "flex-end",
+            //maxWidth: 500,
+
+            borderRadius: 20,
+          }}
+          key={index}
+        >
+          <Text style={{ fontSize: 16, color: "#fff" }} key={index}>
+            {item.text}
+          </Text>
+
+          <View style={styles.rightArrow}></View>
+
+          <View style={styles.rightArrowOverlap}></View>
+        </View> */}
         </View>
 
         <View style={styles.bottomContainer}>
@@ -412,6 +439,7 @@ const styles = StyleSheet.create({
     height: 320,
     bottom: 0,
   },
+  // old speech
   item: {
     marginVertical: moderateScale(7, 2),
     flexDirection: "row",
@@ -456,4 +484,51 @@ const styles = StyleSheet.create({
   arrowRight: {
     right: moderateScale(-6, 0.5),
   },
+
+  //New speech bubble
+  // rightArrow: {
+  //   position: "absolute",
+  //   backgroundColor: "#0078fe",
+  //   //backgroundColor:"red",
+  //   width: 20,
+  //   height: 25,
+  //   bottom: 0,
+  //   borderBottomLeftRadius: 25,
+  //   right: -10
+  // },
+
+  // rightArrowOverlap: {
+  //   position: "absolute",
+  //   backgroundColor: "#eeeeee",
+  //   //backgroundColor:"green",
+  //   width: 20,
+  //   height: 35,
+  //   bottom: -6,
+  //   borderBottomLeftRadius: 18,
+  //   right: -20
+
+  // },
+
+  // /*Arrow head for recevied messages*/
+  // leftArrow: {
+  //   position: "absolute",
+  //   backgroundColor: "#dedede",
+  //   //backgroundColor:"red",
+  //   width: 20,
+  //   height: 25,
+  //   bottom: 0,
+  //   borderBottomRightRadius: 25,
+  //   left: -10,
+  // },
+
+  // leftArrowOverlap: {
+  //   position: "absolute",
+  //   backgroundColor: "#eeeeee",
+  //   //backgroundColor:"green",
+  //   width: 20,
+  //   height: 35,
+  //   bottom: -6,
+  //   borderBottomRightRadius: 18,
+  //   left: -20,
+  // },
 });
